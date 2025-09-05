ac7193f7743623d17aad8cb47836fdbe1c4c24d8
Alireza-Hamidi2001 <alireza.hamidi.eng@gmail.com>
2025-09-05 17:19:14 +0330

    transfer handler

--- scripts/script.js
+++ scripts/script.js
@@ -32,19 +32,78 @@ const summary_LOGOUT = document.querySelector(".summary__span-time");
 // BOX
 const movements__box = document.querySelector(".one");
 
-welcomeEl.innerHTML = `
-            <svg class="icon__bubble">
-                <use xlink:href="./SVGs/sprite.svg#icon-bubbles2"></use>
-            </svg>
-            <p class='welcome__animation'>Hey there . Welcome to bankist project , log in first .</p>`;
+// MODDAL
+let overlayEl = document.querySelector(".overlay");
+let modalEl = document.querySelector(".modal");
 
-balance__money.textContent = "2,555.00 $";
+// // // // // MODAL__section
+const MODAL__handler = function () {
+    overlayEl.style.display = "block";
+    modalEl.style.display = "block";
+    setTimeout(() => {
+        overlayEl.style.display = "none";
+        modalEl.style.display = "none";
+        modalEl.classList.remove("greenBg");
+        modalEl.classList.remove("redBg");
+    }, 4000);
+};
+welcomeEl.innerHTML = `
+    <div>
+        <svg class="icon__bubble">
+        <use xlink:href="./SVGs/sprite.svg#icon-bubbles2"></use>
+        </svg>
+        <div class="welcome__animation">
+        Hey there. Welcome to bankist project, log in first.
+        </div>
+    </div>
+    <div class="guides">
+        <p class="guide">user: js , pin: 1111</p>
+        <p class="guide">user: ah , pin: 2222</p>
+        <p class="guide">user: fh , pin: 3333</p>
+        <p class="guide">user: ss , pin: 4444</p>
+    </div>
+`;
 summary_LOGOUT.textContent = "00:00";
 
-const displayMovements = function (movements, dates) {
+// // // //  WELCOME__section
+const welcome_SECTION = function (selectedAccount) {
+    welcomeEl.innerHTML = `
+        <svg class="icon__bubble">
+        <use xlink:href="./SVGs/sprite.svg#icon-bubbles2"></use>
+        </svg>
+        <p>Welcome back, ${selectedAccount.owner.split(" ")[0]} .</p>`;
+
+    app__container.classList.add("show");
+    console.log(selectedAccount.id);
+};
+
+// // // // ERROR__section
+const error_SECTION = function () {
+    welcomeEl.innerHTML = `
+        <svg class="icon__bubble">
+        <use xlink:href="./SVGs/sprite.svg#icon-bubbles2"></use>
+        </svg>
+        <p>Username or Password is invalid .</p>`;
+};
+
+const summary__SECTION = function (movements) {
+    const positive__movements = movements
+        .filter((posMov) => posMov > 0)
+        .reduce((acc, cur) => acc + cur, 0);
+    const negative__movements = movements
+        .filter((posMov) => posMov < 0)
+        .reduce((acc, cur) => acc + cur, 0);
+    summary_IN.textContent = positive__movements;
+    summary_OUT.textContent = Math.abs(negative__movements);
+    const balance_VALUE = positive__movements + negative__movements;
+    balance__money.textContent = balance_VALUE + " $";
+};
+
+// // // //  DISPALY__MOVEMENTS__section
+const display_Movements = function (movements, dates) {
     movements.map(function (mov, index) {
         const status = mov < 0;
-        const HTMLmovement = `
+        const HTMLmovement = `  
         <div class="box__cashMovement">
             <div class="box__status ${
                 status ? "box__status-withdraw" : "box__status-deposite"
@@ -55,16 +114,8 @@ const displayMovements = function (movements, dates) {
         movements__box.innerHTML += HTMLmovement;
     });
 
-    // -Note- calc the IN & OUT summary
-    const positive__movements = movements
-        .filter((posMov) => posMov > 0)
-        .reduce((acc, cur) => acc + cur, 0);
-    const negative__movements = movements
-        .filter((posMov) => posMov < 0)
-        .reduce((acc, cur) => acc + cur, 0);
-    summary_IN.textContent = positive__movements;
-    summary_OUT.textContent = Math.abs(negative__movements);
-    summary_INTEREST.textContent = positive__movements + negative__movements;
+    // SECTION__SUMMARY
+    summary__SECTION(movements);
 };
 // displayMovements(account1.movements, account1.date);
 
@@ -77,40 +128,29 @@ accounts.map(
             .join("")),
 );
 
-/////////////////////////////////////// -SECTION-
-// HANDLE LOGIN
-const login = (username, pin) => {
+//////////////// -SECTION-
+// // // // // // SECTION LOGIN
+const login_SECTION = (username, pin) => {
     const selectedAccount = accounts.find(
         (curAccount) => curAccount.userName === username,
     );
-    console.log(selectedAccount.id);
 
     if (selectedAccount?.pin === pin) {
-        // نمایش موومنت‌ها بعد از لاگین موفق
-        // -Note- SHOWING THE UI
-        displayMovements(selectedAccount.movements, selectedAccount.date);
-
-        welcomeEl.innerHTML = `
-        <svg class="icon__bubble">
-        <use xlink:href="./SVGs/sprite.svg#icon-bubbles2"></use>
-        </svg>
-        <p>Welcome back, ${selectedAccount.owner.split(" ")[0]} .</p>`;
-
-        app__container.classList.add("show");
+        // 01- show WELCOME
+        welcome_SECTION(selectedAccount);
+        // 02- show MOVEMENTS
+        display_Movements(selectedAccount.movements, selectedAccount.date);
     } else {
-        welcomeEl.innerHTML = `
-        <svg class="icon__bubble">
-        <use xlink:href="./SVGs/sprite.svg#icon-bubbles2"></use>
-        </svg>
-        <p>Username or Password is invalid .</p>`;
+        // 01- ERROR section
+        error_SECTION();
     }
 };
 
 form__btn.addEventListener("click", (e) => {
     e.preventDefault();
-    const username__value = form__username.value;
-    const pin__value = Number(form__pin.value);
-    // guard class
+    let username__value = form__username.value;
+    let pin__value = Number(form__pin.value);
+    // GUARD__class
     if (!username__value || !pin__value) {
         form__username.classList.add("warning");
         form__pin.classList.add("warning");
@@ -122,6 +162,49 @@ form__btn.addEventListener("click", (e) => {
 
         // return;
     } else {
-        login(username__value, pin__value);
+        login_SECTION(username__value, pin__value);
+        form__username.value = "";
+        form__pin.value = "";
     }
 });
+
+// // // // // // // // -SECTION-
+// // // TRANSFER__section
+transfer__btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const balance__cur__acc = Number(balance__money.textContent.split(" ")[0]);
+    const transfer__value = Number(transfer__amount.value);
+    const transfer__receiver = accounts.find(
+        (account) =>
+            account.owner.split(" ")[0].toLocaleLowerCase() ===
+            transfer__to.value,
+    );
+    // // // GUARD__class
+    if (!transfer__value || !transfer__receiver) {
+        transfer__amount.classList.add("warning");
+        transfer__to.classList.add("warning");
+        setTimeout(() => {
+            transfer__amount.classList.remove("warning");
+            transfer__to.classList.remove("warning");
+        }, 2000);
+    } else if (transfer__receiver && balance__cur__acc >= transfer__value) {
+        modalEl.innerHTML = `
+        <p class="showModal">
+        &#10003; Transferred value ${transfer__value} to ${transfer__receiver.owner.split(" ")[0]}
+        </p>`;
+        MODAL__handler();
+        modalEl.classList.add("greenBg");
+    } else if (!transfer__receiver || balance__cur__acc < transfer__value) {
+        modalEl.innerHTML = `
+        <p class="showModal">
+        &#10007; Wrong the Receiver name or Value .
+        </p>`;
+        MODAL__handler();
+        modalEl.classList.add("redBg");
+    }
+    transfer__amount.value = "";
+    transfer__to.value = "";
+});
+
+// console.log(transfer__value, transfer__receiver);
+// console.log(transfer__receiver.owner.split(" ")[0]);
